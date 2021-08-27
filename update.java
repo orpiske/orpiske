8924b832865a6be094ffa381ffeceb0cb79e96b9
Otavio Rodolfo Piske <opiske@redhat.com>
2021-08-27 14:30:52 +0200

    Testing profile updates

--- update.java
+++ update.java
@@ -56,7 +56,7 @@ public class update implements QuarkusApplication {
 
                 Posts about his experiences and troubles with computers, ocasionally appear in his blogs in https://orpiske.net[English] and https://angusyoung.org[Portuguese].
 
-                He is excited about Open Source and contributes all sorts of projects. He is a regular committer at the https://camel.apache.org[Apache Camel] project, and makes or has made sporadic contributions to Fedora, Gentoo, Eclipse Paho, Apache ActiveMQ and others.
+                He is excited about Open Source and contributes all sorts of projects. He is a regular committer at the https://camel.apache.org[Apache Camel] project, and makes or has made sporadic contributions to https://getfedora.org[Fedora], https://gentoo.org[Gentoo], https://www.eclipse.org/paho/[Eclipse Paho], https://activemq.apache.org[Apache ActiveMQ] and others.
 
                 He can be found on twitter as https://twitter.com/otavio021[@otavio021], speaking in English :uk: and Portuguese :brazil: about technology, science and life. He maintains a professional profile on https://www.linkedin.com/in/orpiske/[LinkedIn].
                 """;
